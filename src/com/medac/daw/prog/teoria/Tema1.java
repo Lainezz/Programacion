4161aa1a17a688cc3cc61001ea4917c1a59b4d55
Lainezz <diegolinaresmojacar@gmail.com>
2022-05-31 10:29:23 +0200

    Hola Mundo

---
+++ src/com/medac/daw/prog/teoria/Tema1.java
@@ -0,0 +1,11 @@
+package com.medac.daw.prog.teoria;
+
+public class Tema1 {
+
+	public static void main(String[] args) {
+		//Primer programa con Java
+		System.out.println("Hola Mundo");
+
+	}
+
+}
