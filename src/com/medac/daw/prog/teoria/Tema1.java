24a082f27fb42f718f292a4ca3064098643ead1c
Lainezz <diegolinaresmojacar@gmail.com>
2022-05-31 10:43:35 +0200

    commit de prueba

--- src/com/medac/daw/prog/teoria/Tema1.java
+++ src/com/medac/daw/prog/teoria/Tema1.java
@@ -5,7 +5,9 @@ public class Tema1 {
 	public static void main(String[] args) {
 		//Primer programa con Java
 		System.out.println("Hola Mundo");
-
+		
+		//Prueba para descargar
+		System.out.println("Esto es una prueba");
 	}
 
 }
